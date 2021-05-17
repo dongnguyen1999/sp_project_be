4a7ffc689db0682aed5427507571d17cc5d07e6b
Dong Nguyen <nvdkg1999@gmail.com>
2021-05-17 14:20:52 +0700

    Fix truncate response script

--- sp_project/database_scripts/init_data_student.sql
+++ sp_project/database_scripts/init_data_student.sql
@@ -1,3 +1,10 @@
 -- SQLite
-DELETE FROM TrnQuest;
-DELETE FROM TrnQuestPart;
+INSERT INTO Student (StudID, Name, NRIC, ClassID)
+VALUES 
+  (12345, 'Ah Beng Teo', null, 1),
+  (54321, 'Ah Seng Lim', null, 1),
+  (54322, 'A Dong Teo', null, 1),
+  (21445, 'Tan Lian', null, 2),
+  (21346, 'Chen Kim Huat', null, 3),
+  (54323, 'A Lee Teolim', null, 2),
+  (54324, 'A Lin Seng', null, 2); 
--- sp_project/database_scripts/truncate_response.sql
+++ sp_project/database_scripts/truncate_response.sql
@@ -1,8 +1,3 @@
 -- SQLite
-DROP TABLE Class;
-DROP TABLE Question;
-DROP TABLE QuestionPart;
-DROP TABLE Student;
-DROP TABLE TrnQuest;
-DROP TABLE TrnQuestPart;
-DROP TABLE QuestionAns;
+DELETE FROM TrnQuest;
+DELETE FROM TrnQuestPart;
