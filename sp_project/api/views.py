2c54f43c85e7b7fbc2dd130d1cb1513ed35c4332
leminhhieuctvn <34200894+leminhhieuctvn@users.noreply.github.com>
2021-06-04 17:57:25 +0700

    Le Minh Hieu, 4/6/2021

--- sp_project/api/views.py
+++ sp_project/api/views.py
@@ -1,3 +1,6 @@
+import os
+from collections import defaultdict,OrderedDict
+from django.apps import apps
 from django.db import IntegrityError, transaction
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -25,6 +28,17 @@ def index(request):
 class UploadFileApiView(APIView):
   
   # permission_classes = [permissions.IsAuthenticated]
+  def delete(self, request, *args, **kwargs):
+    data=json.loads(request.body)
+    if data["type"] == 0:
+      data_return=cancel(data["hash"])
+    elif data["type"] == 1:
+      data_return=clear_log(data["hash"])
+    else:
+      error_code = 1
+      msg = "Invalid type!"
+      data_return = {"errorCode": error_code, "message": msg}
+    return Response(data_return)
 
   def get(self, request, *args, **kwargs):
     class_name = request.GET['squad_name']
@@ -36,15 +50,20 @@ class UploadFileApiView(APIView):
       data_return = process_response_set(request)
     if file_type == 'marking-scheme':
       data_return = process_marking_scheme(request)
+    if file_type =='cancel':
+      cancel(request)
+      data_return = "Done"
     return Response(data_return)
 
 def squad_name_check(class_name):
   return not Class.objects.filter(name=class_name)
 
+
 def process_response_set(request):
   file= request.FILES['file']
   class_name = request.POST['squad_name']
-
+  hash = request.POST['hash']
+  dict_json = defaultdict(list)
   if not file.name.endswith(('.xlsx','.xls')):
     raise ValidationError('Neeed Excel File!')
   else:
@@ -57,7 +76,6 @@ def process_response_set(request):
     error_studid = set()
     for row in range(data.shape[0]):
       studid = data.iat[row, 3]
-      print(studid)
       if Student.objects.filter(studid=studid).values('studid'):
         error_studid.add(studid)
         error_code = -1
@@ -65,6 +83,9 @@ def process_response_set(request):
       try:
         with transaction.atomic():
             class_table.save()
+            classid = Class.objects.filter(name=class_name).values('classid')
+            dict_json['Class'].append({'classid':classid[0]['classid']})
+            dict_json.update()
       except IntegrityError:
             transaction.rollback()
             pass
@@ -74,11 +95,11 @@ def process_response_set(request):
             studid = data.iat[row,3]
             name = str(data.iat[row,0]) + str(data.iat[row,1])
             index_temp = 12
-            classid = Class.objects.filter(name=class_name).values('classid')
             student = Student(studid=studid, classid= classid, name=name)
             try:
               with transaction.atomic():
                 student.save()
+                dict_json['Student'].append({'studid': int(studid)})
             except IntegrityError:
               error_code = 1
               error_line.add(row+1)
@@ -96,6 +117,7 @@ def process_response_set(request):
                 try:
                   with transaction.atomic():
                     trnquest.save()
+                    dict_json['TrnQuest'].append({'studid':int(studid), 'questionid':int(questionid)})
                 except IntegrityError:
                   error_code = 1
                   error_line.add(row+1)
@@ -108,6 +130,7 @@ def process_response_set(request):
                 try:
                   with transaction.atomic():
                     trnquestpart.save()
+                    dict_json['TrnQuestPart'].append({'studid':int(studid), 'questionid':int(questionid), 'partid':partid})
                 except IntegrityError:
                   error_code = 1
                   error_line.add(row+1)
@@ -122,13 +145,15 @@ def process_response_set(request):
     msg = "Insert fail at row(s): " + str(error_line)
   else:
     msg = "Insert successful"
-
+    with open(hash+".json","w") as file:
+      json.dump(dict_json,file)
   data_return = {"errorCode": error_code, "message": msg}
   return data_return
 
 def process_marking_scheme(request):
   file= request.FILES['file']
-
+  hash = request.POST['hash']
+  dict_json = defaultdict(list)
   if not file.name.endswith(('.xlsx','.xls')):
     raise ValidationError('Neeed Excel File!')
   else:
@@ -151,6 +176,7 @@ def process_marking_scheme(request):
       try:
         with transaction.atomic():
           questionans.save()
+          dict_json['QuestionAns'].append({'questionid':int(questionid), 'ansid': ansid_temp})
       except IntegrityError:
         error_code = 1
         error_line.add(row+1)
@@ -170,13 +196,13 @@ def process_marking_scheme(request):
         pass
       if ansid_temp > 1:
         ansid_temp = QuestionPartAns.objects.aggregate(Max('partansid'))['partansid__max'] +1
-        print(ansid_temp)
       modelans = data_filtered.iat[row, 3]
       ansmark = data_filtered.iat[row, 4]
       questionpartans = QuestionPartAns(questionid=questionid, partid=partid ,partansid=ansid_temp, modelans=modelans, ansmark=ansmark)
       try:
         with transaction.atomic():
           questionpartans.save()
+          dict_json['QuestionPartAns'].append({'questionid': int(questionid), 'partid': partid, 'ansid':ansid_temp})
       except IntegrityError:
         error_code = 1
         error_line.add(row + 1)
@@ -188,6 +214,34 @@ def process_marking_scheme(request):
     msg = "Insert fail at row(s): " + str(error_line)
   else:
     msg = "Insert successful"
-
+    with open(hash+".json","w") as file:
+      json.dump(dict_json,file)
   data_return = {"errorCode": error_code, "message": msg}
+  return data_return
+
+def clear_log(hash):
+  file = hash + ".json"
+  if os.path.exists(file):
+    os.remove(file)
+    error_code = 0
+    msg = "Done"
+  else:
+    error_code = 1
+    msg = "The log file does not exist"
+  data_return = {"errorCode": error_code, "message": msg}
+  return data_return
+
+
+
+def cancel(hash):
+  file = hash + ".json"
+  with open(file) as datafile:
+    data = json.load(datafile)
+    data = OrderedDict(sorted(data.items(),reverse=True))
+    print(data)
+  for key in data.keys():
+    for i in range(0,len(data[key])):
+      model = apps.get_model('api',key)
+      model.objects.filter(**data[key][i]).delete()
+  data_return = clear_log(hash)
   return data_return
